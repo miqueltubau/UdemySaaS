f7f70cc92d465042985a9452cec38fe0688a95ec
Miquel Tubau Pires <miq.tubau@gmail.com>
2024-03-03 11:53:20 +0100

    [BE]: Add MongoDB connection

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose')
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//connectt our database
+mongoose.connect(process.env.MONGO_URI).then((res) => {console.log('Connected to MongoDB')}).catch((err) => { throw err });
 
 const port = process.env.PORT || 4242;
 
